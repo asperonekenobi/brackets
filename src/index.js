296ecd7148038bf337a53b38c015e1c2a000e3d8
AndreySinkovets <andreysinkovets@yahoo.com>
2020-03-19 00:22:25 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let arrStr = str.split(''),
+        stack = [];
+
+    if (arrStr.length % 2 !== 0) {
+        return false;
+    }
+
+    for (let i = 0; i < arrStr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            /* Проверяем закрывающую и стэк, а затем открывающую т.к есть одинаковые '|' и '|', и если сразу проверять
+            откр. значение повторно заносится в стек*/
+            if (arrStr[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]) {
+
+                stack.pop();
+
+            } else if (arrStr[i] === bracketsConfig[j][0]) {
+                stack.push(arrStr[i]);
+            }
+        }
+    }
+    return stack.length === 0;
+    
+};
